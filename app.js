24d6bcda5afb6ccf8957c1bc8dfa9a1dffce9327
tallpark <tallspark@naver.com>
2022-12-14 19:16:04 +0900

    test2

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
  res.send('Hello World!');
 });
 
+
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
 });
--- routes/goods.js
+++ routes/goods.js
@@ -113,4 +113,6 @@ router.post("/goods/", async (req,res) => {
   res.json({goods: createdGoods});
 })
 
+
+
 module.exports = router;
